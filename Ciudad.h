00566b8d3c25948b2f2a1d31a59554c0ea1ee72b
Keyteer <114495829+Keyteer@users.noreply.github.com>
2023-06-08 16:32:16 -0400

    ciudades

---
+++ Ciudad.h
@@ -0,0 +1,16 @@
+#include <string>
+
+using namespace std;
+
+class Ciudad
+{
+private:
+public:
+	string Country,City,AccentCity;
+	int region, Population,Latitude,Longitude;
+
+	void readCityData(string str);
+
+	Ciudad(/* args */);
+	~Ciudad();
+};

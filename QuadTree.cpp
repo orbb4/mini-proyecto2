d40065deac1237da80e891d6e530994cd45674ab
Basito0 <76927670+Basito0@users.noreply.github.com>
2023-06-08 15:20:27 -0400

    Update QuadTree.cpp

--- QuadTree.cpp
+++ QuadTree.cpp
@@ -1,34 +1,41 @@
 #include "QuadTree.h"
+#include "Node.h"
 #include <iostream>
 
-QuadTree::QuadTree(Point(int x1, int y1) a, Point(int x2, y2) b){
+QuadTree::QuadTree(Point a, Point b){
+	root = nullptr;
+	root = new Node(std::pair(a, b), "", -1, false);
 
 }
 
 QuadTree::~QuadTree(){
-
+	return;
 }
 
 int QuadTree::totalPoints(){
-
+	return 1;
 }
 
 int QuadTree::totalNodes(){
-
+	return 1;
 }
 
 void QuadTree::insert(Point p, int data){
-
+	return;
 }
 
 void QuadTree::list(){
-
+	return;
 }
 
 int QuadTree::countRegion(Point p, int d){
-
+	return 1;
 }
 
 int QuadTree::AggregateRegion(Point p, int d){
+	return 1;
+}
 
+int main(){
+	return 0;
 }

5dc9df8a83d0479f77f79a0adb42d32572faeb8d
Basito0 <76927670+Basito0@users.noreply.github.com>
2023-06-08 13:50:46 -0400

    Add files via upload

---
+++ QuadTree.cpp
@@ -0,0 +1,34 @@
+#include "QuadTree.h"
+#include <iostream>
+
+QuadTree::QuadTree(){
+
+}
+
+QuadTree::~QuadTree(){
+
+}
+
+int QuadTree::totalPoints(){
+
+}
+
+int QuadTree::totalNodes(){
+
+}
+
+void QuadTree::insert(Point p, int data){
+
+}
+
+void QuadTree::list(){
+
+}
+
+int QuadTree::countRegion(Point p, int d){
+
+}
+
+int QuadTree::AggregateRegion(Point p, int d){
+	
+}

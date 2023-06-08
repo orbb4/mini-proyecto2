c7c62112c3f0baeee8f9398abea52fdb494711ec
orbb4 <114522263+orbb4@users.noreply.github.com>
2023-06-08 13:58:45 -0400

    Add files via upload

--- Node.h
+++ Node.h
@@ -1,9 +1,20 @@
+#include <iostream>
+#include <utility>
+
+struct Point{
+	int x;
+	int y;
+};
+
 class Node{
 public:
+
+	Point cords;
+	Node(pair <int[2], int[2]> limites, string ciudad, int poblacion, bool es_negro);
 	Node[4] nodes;
 	// par de ptos que representa los limites del cuadrante del nodo
 	// cada int[2] tiene: (cord superior izquierda, cord inferior derecha)
-	pair <int[2], int[2]> limites;
+	pair <Point p1, Point p2> limites;
 	string ciudad;
 	int poblacion;
 	bool es_negro;

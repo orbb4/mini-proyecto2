54d54bc4eede7595798a84bcfaab738138de3ea1
Basito0 <76927670+Basito0@users.noreply.github.com>
2023-06-19 15:50:10 -0400

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -10,3 +10,13 @@ Node<T>::Node(pair<Point, Point> limites, bool es_negro, T data) {
 	Node nodesAux[4];
 	nodes = nodesAux;
 }
+
+template <class T>
+
+bool Node<T>::inBoundary(Point p)
+{
+    Point topLeft = std::get<0>(limites);
+    Point botRight = std::get<1>(limites);
+    return (p.x >= topLeft.x && p.x <= botRight.x
+        && p.y >= topLeft.y && p.y <= botRight.y);
+}
